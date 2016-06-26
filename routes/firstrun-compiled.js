e6c5132b1fc9222ed1c5b35908489b10eabac721
derwish-pro <derwish.pro@gmail.com>
2016-06-26 15:53:50 +0300

    include bower

---
+++ routes/firstrun-compiled.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+	res.render('firstRun/index', {title: 'First Run'});
+});
+
+module.exports = router;
+
+//# sourceMappingURL=firstrun-compiled.js.map
