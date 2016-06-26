5b3a69c4d73bfd555e9ef8b97fe58786314c0a3e
derwish-pro <derwish.pro@gmail.com>
2016-06-26 20:36:50 +0300

    first run: database config

--- routes/firstrun.js
+++ routes/firstrun.js
@@ -2,11 +2,16 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function (req, res, next) {
-	res.render('firstRun/index');
+	res.render('FirstRun/index');
 });
 
 router.get('/Database', function (req, res, next) {
-	res.render('FirstRun/database');
+	res.render('FirstRun/Database/index');
 });
+
+router.get('/Database/External', function (req, res, next) {
+	res.render('FirstRun/Database/external');
+});
+
 
 module.exports = router;
