7c56ae2a2a394492d884f559988ccb0721f1c221
derwish-pro <derwish.pro@gmail.com>
2016-06-26 21:15:53 +0300

    first run: gateway

--- routes/firstrun.js
+++ routes/firstrun.js
@@ -13,5 +13,17 @@ router.get('/Database/External', function (req, res, next) {
 	res.render('FirstRun/Database/external');
 });
 
+router.get('/Gateway', function (req, res, next) {
+	res.render('FirstRun/Gateway/index');
+});
+
+router.get('/Gateway/Ethernet', function (req, res, next) {
+	res.render('FirstRun/Gateway/ethernet');
+});
+
+router.get('/Gateway/Serial', function (req, res, next) {
+	res.render('FirstRun/Gateway/serial');
+});
+
 
 module.exports = router;
