005730742ab625a9268c7cbcb5d4180e31a7b94b
derwish-pro <derwish.pro@gmail.com>
2016-06-26 17:14:00 +0300

    first run: database page

--- routes/firstrun.js
+++ routes/firstrun.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', function (req, res, next) {
 	res.render('firstRun/index', {title: 'First Run'});
+});
+
+router.get('/Database', function (req, res, next) {
+	res.render('firstRun/database', {title: 'First Run'});
 });
 
 module.exports = router;
