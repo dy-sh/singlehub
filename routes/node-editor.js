327998fb4a083b1e654f42fde98fa39877b0cae5
derwish-pro <derwish.pro@gmail.com>
2016-07-02 02:33:16 +0300

    node editor router

--- routes/node-editor.js
+++ routes/node-editor.js
@@ -4,11 +4,68 @@
 
 var express = require('express');
 var router = express.Router();
-//var config = require('./../config');
+var config = require('./../config');
+var engine = require('../modules/nodes/nodes-engine');
+
+var MAIN_PANEL_ID = "Main";
+
+router.get('/', function (req, res) {
+
+	//todo if (engine.isNotStarted)
+	//	res.render("Error", "Nodes Engine is not started.<br/><br/>   <a href='/Config'>Check settings</a>");
 
 
-router.get('/', function (req, res, next) {
-	res.render('node-editor');
+	res.render('node-editor', {
+		split: req.params.split,
+		panelId: MAIN_PANEL_ID,
+		theme: config.nodeEditor.theme
+	});
+});
+
+
+router.get('/Panel', function (req, res) {
+
+	//todo if (engine.isNotStarted)
+	//	res.render("Error", "Nodes Engine is not started.<br/><br/>   <a href='/Config'>Check settings</a>");
+
+	if (!req.params.panelId || req.params.panelId == MAIN_PANEL_ID)
+		res.redirect("/NodeEditor");
+
+	var panel = engine.getPanelNode(req.params.panelId);
+	if (!panel)
+		res.status(404).send('Panel not found!');
+
+	var panelsStack = {};
+	var p = panel;
+	while (true) {
+		panelsStack[p.id] = p.name;
+		if (p.panelId == MAIN_PANEL_ID)
+			break;
+		p = engine.getPanelNode(p.panelId);
+	}
+
+	res.render('node-editor', {
+		split: req.params.split,
+		panelId: panel.Id,
+		ownerPanelId: panel.panelId,
+		panelsStack: panelsStack,
+		theme: config.nodeEditor.theme
+	});
+});
+
+
+router.get('/Split', function (req, res) {
+	//todo if (engine.isNotStarted)
+	//	res.render("Error", "Nodes Engine is not started.<br/><br/>   <a href='/Config'>Check settings</a>");
+
+	var route = req.params.panelId ? "Panel/" + req.params.panelId : "";
+
+	res.render('split', {route: route});
+});
+
+
+router.get('/NodesDescription', function (req, res) {
+	//todo
 });
 
 module.exports = router;
