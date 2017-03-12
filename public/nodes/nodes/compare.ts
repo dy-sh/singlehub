c42e1a8b39fa218ef44db59522d88effa6dcb998
derwish-pro <derwish.pro@gmail.com>
2017-03-13 02:01:52 +0300

    EqualNode

--- public/nodes/nodes/compare.ts
+++ public/nodes/nodes/compare.ts
@@ -59,3 +59,32 @@ class OrNode extends Node {
 }
 Container.registerNodeType("compare/or", OrNode);
 
+
+
+class EqualNode extends Node {
+    constructor() {
+        super();
+
+        this.title = "Equal";
+        this.descriprion = "This node compares two values and sends \"true\" to the output " +
+            "if the values are equal, or \"false\" if not equal. <br/>" +
+            "It can compare text or numbers. <br/>" +
+            "For example, the node will assume that \"1\" and \"1.0\" are equal. <br/>" +
+            "\"Hello\" and \"HELLO\" are not equal. ";
+
+        this.addInput("a");
+        this.addInput("b");
+        this.addOutput("a == b", "boolean");
+    }
+
+    onInputUpdated() {
+        let a = this.getInputData(0);
+        let b = this.getInputData(1);
+
+        if (a != null && b != null)
+            this.setOutputData(0, a == b);
+        else
+            this.setOutputData(0, null);
+    }
+}
+Container.registerNodeType("compare/equal", EqualNode);
