aaf399ab1331bbb488d7c0382f6db7475e964d0b
derwish-pro <derwish.pro@gmail.com>
2017-05-20 00:21:51 +0300

    update in db

--- src/public/nodes/nodes/protocols/xiaomi/devices/power-plug.ts
+++ src/public/nodes/nodes/protocols/xiaomi/devices/power-plug.ts
@@ -4,9 +4,16 @@ export default class PowerPlug implements IXiomiDeviceModel {
     onCreate(node: XiaomiDeviceNode): void {
         node.addInput("power", "boolean");
         node.addOutput("power", "boolean");
+
+        //update db
+        if (node.container.db)
+            node.container.db.updateNode(node.id, node.container.id, {
+                $set: { inputs: node.inputs, outputs: node.outputs }
+            });
     }
 
     onInputUpdated(node: XiaomiDeviceNode): void {
+        //send power state to device
         if (node.device && node.inputs[1].updated) {
             node.device.setPower(0, node.inputs[1].data == true); //convert type to bool, prevent null
         }
