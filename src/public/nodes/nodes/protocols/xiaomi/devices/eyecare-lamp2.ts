ba8c83a9dec2356ef603ea79de7f0e159b0f7d74
derwish-pro <derwish.pro@gmail.com>
2017-06-10 00:35:13 +0300

    set bright

--- src/public/nodes/nodes/protocols/xiaomi/devices/eyecare-lamp2.ts
+++ src/public/nodes/nodes/protocols/xiaomi/devices/eyecare-lamp2.ts
@@ -5,12 +5,24 @@ export default class EyecareLamp2 implements IXiomiDeviceModel {
     onCreate(node: XiaomiDeviceNode): void {
         node.addInput("power", "boolean");
         node.addOutput("power", "boolean");
+        node.addInput("bright", "number");
+        node.addOutput("bright", "number");
     }
 
     onInputUpdated(node: XiaomiDeviceNode): void {
         //send power state to device
         if (node.device && node.inputs[1].updated) {
             node.device.setPower(node.inputs[1].data == true); //convert type to bool, prevent null
+        }
+
+        //send bright to device
+        if (node.device && node.inputs[2].updated) {
+            var val = node.inputs[2].data;
+            if (val != null) {
+                if (val < 1) val = 1;
+                if (val > 100) val = 100;
+                node.device.setBrightness(val);
+            }
         }
     }
 
