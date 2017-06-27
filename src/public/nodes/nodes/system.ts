eb7c55348f233a93387cf8e252517d5eb336fae4
derwish-pro <derwish.pro@gmail.com>
2017-06-28 00:38:06 +0300

    SystemJsonFileNode

--- src/public/nodes/nodes/system.ts
+++ src/public/nodes/nodes/system.ts
@@ -131,3 +131,93 @@ export class SystemFileNode extends Node {
     };
 }
 Container.registerNodeType("system/file", SystemFileNode);
+
+
+
+
+
+export class SystemJsonFileNode extends Node {
+
+    constructor() {
+        super();
+        this.title = "Json file";
+        this.descriprion = "This node can read and write Json file on the disk. <br/>" +
+            "Send the file name To the input named File Name. The path can be omitted. <br/>" +
+            "With logic inputs named Read, Write, Delete File you can perform the requested operation. <br/>" +
+            "Specify the key that you want to read/write. <br/>" +
+            "The value that you want to write, send to Value input. <br/>" +
+            "Read value will be sent to the output.";
+
+        this.addInput("file name", "string");
+        this.addInput("key", "string");
+        this.addInput("value", "string");
+        this.addInput("read", "boolean");
+        this.addInput("write", "boolean");
+        this.addInput("delete", "boolean");
+
+        this.addOutput("value", "string");
+
+        // options.ProtectedAccess = true;
+    }
+
+    onInputUpdated() {
+        //read
+        if (this.inputs[3].updated && this.inputs[3].data) {
+            let fileName = this.getInputData(0);
+            if (fileName == null)
+                return this.debugWarn("Cant read file. File name is not defined");
+
+            let key = this.getInputData(1);
+
+            fs.readFile(fileName, 'utf8', (err, data) => {
+                if (err) return this.debugErr(err);
+
+                let obj = JSON.parse(data);
+                let val = obj[key];
+                this.setOutputData(0, val);
+            });
+        }
+
+        //write
+        if (this.inputs[4].updated && this.inputs[4].data) {
+            let fileName = this.getInputData(0);
+            if (fileName == null)
+                return this.debugWarn("Cant write file. File name is not defined");
+
+            let key = this.getInputData(1);
+            let value = this.getInputData(2);
+
+            fs.readFile(fileName, 'utf8', (err, data) => {
+                let obj = {};
+
+                if (data)
+                    obj = JSON.parse(data);
+
+                obj[key] = value;
+                let json = JSON.stringify(obj);
+                fs.writeFile(fileName, json, 'utf8', (err) => {
+                    if (err) return this.debugErr(err);
+
+                    this.debugInfo("The file " + fileName + " was saved!");
+                });
+            });
+        }
+
+        //delete
+        if (this.inputs[5].updated && this.inputs[5].data) {
+            let fileName = this.getInputData(0);
+            if (fileName == null)
+                return this.debugWarn("Cant delete file. File name is not defined");
+
+            fs.unlink(fileName, (err) => {
+                if (err) return this.debugErr(err);
+
+                this.debugInfo("The file " + fileName + " was deleted!");
+            });
+
+        }
+
+
+    };
+}
+Container.registerNodeType("system/json-file", SystemJsonFileNode);
