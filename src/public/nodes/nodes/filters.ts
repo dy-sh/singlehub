af5e16f7d43b085fa565bbdcc2123c7189e60702
derwish-pro <derwish.pro@gmail.com>
2017-08-18 01:07:46 +0300

    FiltersReduceDuplicatesNode

--- src/public/nodes/nodes/filters.ts
+++ src/public/nodes/nodes/filters.ts
@@ -234,13 +234,13 @@ class FiltersPreventEqualNode extends Node {
 Container.registerNodeType("filters/prevent-equal", FiltersPreventEqualNode);
 
 
-class FiltersPreventDuplicationNode extends Node {
+class FiltersPreventDuplicatesNode extends Node {
     lastVal: any;
 
     constructor() {
         super();
 
-        this.title = "Prevent duplication";
+        this.title = "Prevent duplicates";
         this.descriprion = "This node filters the input values. " +
             "It transmits the value only if it is not the same " +
             "that was already sent to the output.";
@@ -260,7 +260,7 @@ class FiltersPreventDuplicationNode extends Node {
         this.setOutputData(0, val);
     }
 }
-Container.registerNodeType("filters/prevent-duplication", FiltersPreventDuplicationNode);
+Container.registerNodeType("filters/prevent-duplicates", FiltersPreventDuplicatesNode);
 
 
 
@@ -279,7 +279,7 @@ class FiltersReduceEventsNode extends Node {
             "the node sends it to the output and stops " +
             "receiving at a specified time interval. <br/>" +
             "The values that come at this time are simply ignored, " +
-            "but the last value will be stored. <br/>" +
+            "but the last value will be stored if it is enabled in the settings. <br/>" +
             "When the interval passes, the last value is sent to the output. " +
             "This reduces the number of events, if they are sent too often, but" +
             "ensures that the node will always send the last actual value. <br/><br/>" +
@@ -287,7 +287,7 @@ class FiltersReduceEventsNode extends Node {
             "This may be necessary if you do not want to receive messages late, " +
             "but then it is not guaranteed that the nodes connected to this node " +
             "will have the last actual value.<br>" +
-            "If you activate reset pin, the node will reset the timer " +
+            "If you activate Reset pin, the node will reset the timer " +
             "and send the last value, if it is enabled in the settings. ";
 
 
@@ -344,3 +344,85 @@ class FiltersReduceEventsNode extends Node {
     }
 }
 Container.registerNodeType("filters/reduce-events", FiltersReduceEventsNode);
+
+
+
+class FiltersReduceDuplicatesNode extends Node {
+    lastTime: number;
+    lastValue: any;
+    waitingToSend: boolean;
+
+    constructor() {
+        super();
+
+        this.title = "Reduce Duplicates";
+        this.descriprion = "This node reduces the number of duplicated transmitted values. <br/><br/>" +
+            "When the value on the input is changed, " +
+            "the node sends it to the output and stops " +
+            "receiving the same value at a specified time interval. <br/>" +
+            "The values that come at this time and that are the same are simply ignored, " +
+            "but the last value will be stored if it is enabled in the settings. <br/>" +
+            "When the interval passes, the last value is sent to the output. " +
+            "This reduces the number of events, if they are sent too often, but" +
+            "ensures that the node will always send the last actual value. <br/><br/>" +
+            "You can disable the sending of the last value in the settings of the node. " +
+            "This may be necessary if you do not want to receive messages late, " +
+            "but then it is not guaranteed that the nodes connected to this node " +
+            "will have the last actual value.<br>" +
+            "If you activate Reset pin, the node will reset the timer " +
+            "and send the last value, if it is enabled in the settings. ";
+
+
+        this.addInput("value");
+        this.addInput("[interval]", "number");
+        this.addInput("[reset]", "boolean");
+        this.addOutput("value");
+
+        this.settings["sendlast"] = { description: "Store and send last value", value: true, type: "boolean" };
+        this.settings["interval"] = { description: "Interval", value: 1000, type: "number" };
+
+        this.lastTime = Date.now();
+    }
+
+    onInputUpdated() {
+        if (this.inputs[2].updated && this.inputs[2].data) {
+            this.lastTime = 0;
+        }
+        if (this.inputs[0].updated) {
+            let interval = this.getInterval();
+
+            if ((Date.now() - this.lastTime) >= interval || this.inputs[0].data != this.lastValue) {
+                this.lastValue = this.inputs[0].data;
+                this.lastTime = Date.now();
+                this.setOutputData(0, this.inputs[0].data)
+            } else {
+                if (this.settings["sendlast"].value == true) {
+                    this.waitingToSend = true;
+                }
+            }
+        }
+    }
+
+    onExecute() {
+        if (!this.waitingToSend)
+            return;
+
+        let interval = this.getInterval();
+
+        if ((Date.now() - this.lastTime) < interval)
+            return;
+
+        this.lastTime = Date.now();
+        this.setOutputData(0, this.lastValue)
+        this.waitingToSend = false;
+    }
+
+    private getInterval(): number {
+        let interval = this.getInputData(1);
+        if (interval == null)
+            interval = this.settings["interval"].value;
+
+        return interval;
+    }
+}
+Container.registerNodeType("filters/reduce-duplicates", FiltersReduceDuplicatesNode);
