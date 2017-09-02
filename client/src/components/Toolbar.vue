917f1c01d2d0d806ce5f47b5e32a9915a5d6d5a0
derwish-pro <derwish.pro@gmail.com>
2017-09-03 02:06:48 +0300

    toolbar

---
+++ client/src/components/Toolbar.vue
@@ -0,0 +1,45 @@
+<template lang='pug'>
+  div
+    v-navigation-drawer(persistent='', :mini-variant='miniVariant', :clipped='clipped', v-model='drawer', enable-resize-watcher='', app='')
+      v-list
+        v-list-tile(v-for='(item, i) in items', :key='i', value='true')
+          v-list-tile-action
+            v-icon(light='', v-html='item.icon')
+          v-list-tile-content
+            v-list-tile-title(v-text='item.title')
+    v-toolbar(fixed='', app='', :clipped-left='clipped' dense)
+      v-toolbar-side-icon(@click.stop='drawer = !drawer')
+      v-btn(icon='', @click.stop='miniVariant = !miniVariant')
+        v-icon(v-html="miniVariant ? 'chevron_right' : 'chevron_left'")
+      v-btn(icon='', @click.stop='clipped = !clipped')
+        v-icon web
+      v-btn(icon='', @click.stop='fixed = !fixed')
+        v-icon remove
+      v-toolbar-title(v-text='title')
+      v-spacer
+      v-btn(icon='', @click.stop='rightDrawer = !rightDrawer')
+        v-icon menu
+</template>
+
+
+<script>
+export default {
+  data() {
+    return {
+      clipped: false,
+      drawer: true,
+      fixed: false,
+      items: [{ icon: "bubble_chart", title: "Inspire" }],
+      miniVariant: false,
+      right: true,
+      rightDrawer: false,
+      title: "Vuetify.js"
+    };
+  }
+};
+</script>
+
+
+<style>
+
+</style>
--- client/src/main.js
+++ client/src/main.js
@@ -1,9 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Toolbar from './components/Toolbar.vue'
 import Vuetify from 'vuetify'
 import './stylus/main.styl'
 
-Vue.use(Vuetify)
+Vue.use(Vuetify);
+
+Vue.component("toolbar", Toolbar);
 
 new Vue({
   el: '#app',
