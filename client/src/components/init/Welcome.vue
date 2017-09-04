f8d4e32249b80c55e347645ede0192f669dbbbd5
derwish-pro <derwish.pro@gmail.com>
2017-09-04 03:07:06 +0300

    stepper

--- client/src/components/init/Welcome.vue
+++ client/src/components/init/Welcome.vue
@@ -2,12 +2,12 @@
   div
     main
       v-content
-        v-container(fluid='')
+        v-container(fluid)
           v-slide-y-transition(mode='out-in')
             v-layout(column align-center)
               h4 First Run
               v-stepper.grey.darken-3(v-model='e1')
-                v-stepper-header(color='grey lighten-1')
+                v-stepper-header.grey.darken-2
                   v-stepper-step(step='1', :complete='e1 > 1') Welcome
                   v-divider
                   v-stepper-step(step='2', :complete='e1 > 2') Database
@@ -15,25 +15,26 @@
                   v-stepper-step(step='3') User
                 v-stepper-content(step='1')
                   v-card(color='grey darken-3')
-                    h5 Welcome to SingleHub
+                    h5.text-xs-center Welcome to SingleHub
                     p This is the initial settings wizard.
                     p You will be offered the following steps:
                       ul
                         li Configure database
                         li Create Admin profile
                   .text-xs-center      
-                    v-btn(color='primary', @click.native='e1 = 2' flat) Well, let's start
+                    v-btn(color='grey darken-1', @click.native='e1 = 2') Well, let's start
                   //- v-btn(flat='') Cancel
                 v-stepper-content(step='2')
-                  v-card(color='grey darken-4', height='200px')
+                  v-card(color='grey darken-3')
                     h5 Welcome to SingleHub
-                      p This is the initial settings wizard.
-                      p You will be offered the following steps:
-                        ul
-                          li Configure database
-                          li Create Admin profile
-                  v-btn(color='primary', @click.native='e1 = 3') Continue
-                  v-btn(flat='') Cancel
+                    p This is the initial settings wizard.
+                    p You will be offered the following steps:
+                      ul
+                        li Configure database
+                        li Create Admin profile
+                  .text-xs-center        
+                    v-btn(@click.native='e1 = 1' flat) Back
+                    v-btn(color='grey darken-1', @click.native='e1 = 3') Continue
                 v-stepper-content(step='3')
                   v-card(color='blue-grey darken-4', height='200px')
                     h5 Welcome to SingleHub
@@ -62,7 +63,7 @@ export default {
 .card {
   margin-bottom: 20px;
   padding: 20px;
-  width: 400px;
+  width: 600px;
   height: 200px;
 }
 </style>
