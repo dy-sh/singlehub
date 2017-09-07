f0ca34867b499466955f42f315308419cbaeb8a2
derwish-pro <derwish.pro@gmail.com>
2017-09-07 22:53:38 +0300

    db wizard

---
+++ client/src/components/init/Database.vue
@@ -0,0 +1,54 @@
+<template lang='pug'>
+main
+  v-content    
+    v-container(grid-list-xl)
+      v-layout(row wrap)
+        v-flex(xs12 sm10 md8 offset-xs0 offset-sm1 offset-md2)
+          //- v-card(dark color='green')
+          v-card(color='grey darken-3')
+            v-toolbar(color='blue darken-2' dense)
+              v-toolbar-title Database setup
+              v-spacer
+              v-btn(dark icon)
+                v-icon view_module
+            v-card-text
+              p Select database. 
+              v-layout(wrap)
+                v-flex(sm6)
+                  h6 External database
+                    br
+                    | (coming soon)
+                  ul
+                    li Requires configuration
+                    li You can use separate server
+                    li Works faster than the built-in database
+                    li Gives a performance boost
+                v-flex(sm6)
+                  h6 Built-in database
+                  ul
+                    li Requires no configuration
+                    li Stored as a file on the disk that can be easily copied or deleted
+              p If you don't know what database to use, choose the built-in database.
+              v-divider(dark)
+              .text-xs-center  
+                v-btn(color='blue darken-2', @click.native='e1 = 2' disabled) External 
+                v-btn(color='blue darken-2', @click.native='e1 = 2') Built-in
+              v-divider(dark)
+              .text-xs-center.pt-2
+                a(href='/first-run/database/none') Skip this step. Don't use a database.
+
+</template>
+
+
+<script>
+export default {
+  data() {}
+};
+</script>
+
+
+<style scoped>
+a {
+  color: gray;
+}
+</style>
--- client/src/routes.js
+++ client/src/routes.js
@@ -1,7 +1,9 @@
-import Admin from "./components/init/Admin.vue"
 import Welcome from "./components/init/Welcome.vue"
+import Database from "./components/init/Database"
+import Admin from "./components/init/Admin.vue"
 
 export default [
     { path: "/", component: Welcome },
+    { path: "/db", component: Database },
     { path: "/admin", component: Admin },
 ]
