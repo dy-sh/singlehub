97e9465a1265b980d0466dfcbcc1c36e3589f779
derwish-pro <derwish.pro@gmail.com>
2017-09-08 03:31:06 +0300

    ui switch node

---
+++ client/src/components/dashboard/UiSwitchNode.vue
@@ -0,0 +1,25 @@
+<template lang='pug'>
+v-list-tile
+  v-list-tile-content
+    v-list-tile-title {{title}}
+    //- v-list-tile-sub-title Hangouts message
+  v-spacer
+  v-list-tile-action
+    v-switch(v-model="sw1" value="sw1" color='grey lighten-3')  
+</template>
+
+
+<script>
+export default {
+  data() {
+    return {
+      title: "Ligth"
+    };
+  }
+};
+</script>
+
+
+<style>
+
+</style>
