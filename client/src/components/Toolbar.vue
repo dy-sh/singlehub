bcf6efa224ce31d2fcfb8d6fea784d0bf130c89a
derwish-pro <derwish.pro@gmail.com>
2017-09-08 03:21:09 +0300

    sidebar

--- client/src/components/Toolbar.vue
+++ client/src/components/Toolbar.vue
@@ -8,18 +8,18 @@ div
         v-list-tile-content
           v-list-tile-title
             | {{ item.text }}
-      v-subheader.mt-3.grey--text.text--darken-1 SUBSCRIPTIONS
-      v-list
-        v-list-tile(v-for='item in items2', :key='item.text', avatar='', @click='')
-          v-list-tile-title(v-text='item.text')
+      v-subheader.mt-3.grey--text.text--darken-1 SETTINGS
+      //- v-list
+      //-   v-list-tile(v-for='item in items2', :key='item.text', avatar='', @click='')
+      //-     v-list-tile-title(v-text='item.text')
       v-list-tile.mt-3(@click='')
         v-list-tile-action
           v-icon(color='grey darken-1') add_circle_outline
-        v-list-tile-title.grey--text.text--darken-1 Browse Channels
+        v-list-tile-title.grey--text.text--darken-1 Hardware
       v-list-tile(@click='')
         v-list-tile-action
           v-icon(color='grey darken-1') settings
-        v-list-tile-title.grey--text.text--darken-1 Manage Subscriptions
+        v-list-tile-title.grey--text.text--darken-1 Settings
   v-toolbar(dense fixed clipped-left app)
     v-toolbar-side-icon(@click.stop='drawer = !drawer')
     v-toolbar-title(v-text='title')
@@ -35,13 +35,13 @@ export default {
     drawer: true,
     title: "SingleHub",
     items: [
-      { icon: "trending_up", text: "Most Popular" },
-      { icon: "subscriptions", text: "Subscriptions" }
+      { icon: "subscriptions", text: "Dashboard" },
+      { icon: "trending_up", text: "Editor" }
       // { icon: "history", text: "History" },
       // { icon: "featured_play_list", text: "Playlists" },
       // { icon: "watch_later", text: "Watch Later" }
-    ],
-    items2: [{ picture: 28, text: "Joseph" }, { picture: 38, text: "Apple" }]
+    ]
+    // items2: [{ picture: 28, text: "Joseph" }, { picture: 38, text: "Apple" }]
   })
 };
 </script>
--- client/src/components/dashboard/Dashboard.vue
+++ client/src/components/dashboard/Dashboard.vue
@@ -18,14 +18,14 @@ main
                   //- v-list-tile-sub-title Hangouts message
                 v-spacer
                 v-list-tile-action
-                  v-switch(v-model="ex15" value="John")
+                  v-switch(v-model="sw1" value="John")
               v-list-tile
                 v-list-tile-content
                   v-list-tile-title Curtains
                   //- v-list-tile-sub-title Hangouts message
                 v-spacer
                 v-list-tile-action
-                  v-btn(color='blue darken-2', @click='submit') ON   
+                  v-btn(color='blue darken-2', @click='') ON   
             v-divider
             v-list(subheader)
               v-subheader Kitchen
@@ -35,74 +35,23 @@ main
                   //- v-list-tile-sub-title Hangouts message
                 v-spacer
                 v-list-tile-action
-                  v-switch(v-model="ex15" value="John")
+                  v-switch(v-model="sw1" value="John")
               v-list-tile
                 v-list-tile-content
                   v-list-tile-title Window
                   //- v-list-tile-sub-title Hangouts message
                 v-spacer
                 v-list-tile-action
-                  v-btn(color='blue darken-2', @click='submit') ON  
-      //- v-layout(row wrap)
-      //-   v-flex(xs12 sm10 md8 offset-xs0 offset-sm1 offset-md2)
-      //-     v-card(color='grey darken-3')
-      //-       v-toolbar(color='blue darken-2' dense)
-      //-         v-toolbar-title Panel
-      //-         v-spacer
-      //-         v-icon(medium) account_box
-      //-       v-card-text
-      //-         div text
-      //-         v-spacer
-      //-         v-btn(color='blue darken-2', @click='submit') ON   
-      //-       v-card-text
-      //-         span text
-      //-         v-spacer     
-      //-         v-switch(label="John" v-model="ex15" value="John")
+                  v-btn(color='blue darken-2', @click='') ON  
+
 </template>
 
 
 <script>
 export default {
   data: () => ({
-    valid: true,
-    name: "",
-    password: "",
-    confirmPassword: "",
-    nameRules: [
-      v => !!v || "Name is required",
-      v => (v && v.length <= 10) || "Name must be less than 10 characters"
-    ],
-    passwordRules: [v => !!v || "Password is required"],
-    confirmPasswordRules: [v => !!v || "Confirm password is required"]
-    // email: "",
-    // emailRules: [
-    //   v => !!v || "E-mail is required",
-    //   v =>
-    //     /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(v) ||
-    //     "E-mail must be valid"
-    // ],
-  }),
-  computed: {
-    matchPassword() {
-      return this.password === this.confirmPassword;
-    }
-  },
-  methods: {
-    submit() {
-      if (this.password !== this.confirmPassword) {
-        this.confirmPassword = "";
-      } else if (this.$refs.form.validate()) {
-        // Native form submission is not yet supported
-        axios.post("/api/submit", {
-          name: this.name
-        });
-      }
-    },
-    clear() {
-      this.$refs.form.reset();
-    }
-  }
+    sw1: true
+  })
 };
 </script>
-
 
