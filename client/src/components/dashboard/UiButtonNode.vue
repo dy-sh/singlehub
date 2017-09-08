394ab07c65ef79c3ab2f61313c0e58d75a604714
derwish-pro <derwish.pro@gmail.com>
2017-09-08 03:45:32 +0300

    ui button node

---
+++ client/src/components/dashboard/UiButtonNode.vue
@@ -0,0 +1,23 @@
+<template lang='pug'>
+  v-list-tile
+    v-list-tile-content
+      v-list-tile-title Curtains
+      //- v-list-tile-sub-title Hangouts message
+    v-spacer
+    v-list-tile-action
+      v-btn(color='blue darken-2', @click='') ON   
+</template>
+
+
+<script>
+export default {
+  data() {
+    return {};
+  }
+};
+</script>
+
+
+<style>
+
+</style>
