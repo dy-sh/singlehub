797cdeb696e294033969f91ffeff4b564041b846
derwish-pro <derwish.pro@gmail.com>
2017-09-13 01:07:09 +0300

    panel

--- client/src/components/dashboard/Dashboard.vue
+++ client/src/components/dashboard/Dashboard.vue
@@ -3,35 +3,20 @@ main
   v-content    
     v-container(grid-list-xl)
       v-layout(row wrap)
-        v-flex(xs12 sm10 md6 offset-xs0 offset-sm1 offset-md3)
-          v-card(color='grey darken-3')
-            v-toolbar(color='blue darken-2' dense)
-              v-toolbar-title My Device
-              v-spacer
-              v-toolbar-side-icon
-            v-divider
-            v-list(subheader)
-              v-subheader Living room
-              uiButtonNode
-              uiSwitchNode
-            v-divider
-            v-list(subheader)
-              v-subheader Kitchen
-              uiButtonNode
-              uiSwitchNode 
+        panel
+        panel
+
 
 </template>
 
 
 <script>
-import UiButtonNode from "./UiButtonNode.vue";
-import UiSwitchNode from "./UiSwitchNode.vue";
+import Panel from "./Panel.vue";
 
 export default {
   data: () => ({}),
   components: {
-    uiButtonNode: UiButtonNode,
-    uiSwitchNode: UiSwitchNode
+    panel: Panel
   }
 };
 </script>
---
+++ client/src/components/dashboard/Panel.vue
@@ -0,0 +1,33 @@
+<template lang='pug'>
+v-flex(xs12 sm10 md6 offset-xs0 offset-sm1 offset-md3)
+  v-card(color='grey darken-3')
+    v-toolbar(color='blue darken-2' dense)
+      v-toolbar-title My Device
+      v-spacer
+      v-toolbar-side-icon
+    v-divider
+    v-list(subheader)
+      v-subheader Living room
+      uiButtonNode
+      uiSwitchNode
+    v-divider
+    v-list(subheader)
+      v-subheader Kitchen
+      uiButtonNode
+      uiSwitchNode 
+</template>
+
+
+<script>
+import UiButtonNode from "./UiButtonNode.vue";
+import UiSwitchNode from "./UiSwitchNode.vue";
+
+export default {
+  data: () => ({}),
+  components: {
+    uiButtonNode: UiButtonNode,
+    uiSwitchNode: UiSwitchNode
+  }
+};
+</script>
+
