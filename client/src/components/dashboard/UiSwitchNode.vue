e114378f5c1f92690e47535f0685af7da9dc3356
derwish-pro <derwish.pro@gmail.com>
2017-09-14 02:21:08 +0300

    switch socket

--- client/src/components/dashboard/UiSwitchNode.vue
+++ client/src/components/dashboard/UiSwitchNode.vue
@@ -22,6 +22,17 @@ export default {
     onClick() {
       this.value = !this.value;
       console.log("click " + this.id + " : " + this.value);
+      this.$socket.emit("node_data", { node: this.id, value: this.value });
+    }
+  },
+  sockets: {
+    connect() {
+      console.log("UiSwitchNode socket connected");
+    },
+    customEmit(val) {
+      console.log(
+        'UiSwitchNode this method was fired by the socket server. eg: io.emit("customEmit", data)'
+      );
     }
   }
 };
