27a7b8b7009611207177bd35bf6629a248a2102b
derwish-pro <derwish.pro@gmail.com>
2017-09-21 02:57:24 +0300

    dashboard

--- src/public/dashboard/dashboard.ts
+++ src/public/dashboard/dashboard.ts
@@ -8,6 +8,7 @@ import { Database } from "../../public/interfaces/database";
 import { Container } from "../nodes/container";
 import { Node } from "../nodes/node";
 import { UiNode } from "../nodes/nodes/ui/ui-node";
+import { UiElement } from "./ui-element";
 
 
 
@@ -18,13 +19,9 @@ export class Dashboard {
     constructor(db: Database) {
         this.db = db;
         this.uiPanels = db.getUiPanels();
-        Container.containers[0].dashboard = this;
-        Container.containers[0].on("created", this.onNodeCreated)
-        Container.containers[0].on("removed", this.onNodeRemoved)
     }
 
     onNodeCreated(node: UiNode) {
-        console.log("onNodeCreated")
         if (!node.isDashboardNode)
             return;
     }
@@ -35,54 +32,83 @@ export class Dashboard {
     }
 
     onNodeChangePanel(oldName: string, newName: string) {
-        console.log("onNodeChangePanel")
+        console.log("onNodeChangePanel", oldName, newName)
+        var oldPanel = this.getUiPanel(oldName);
+        if (oldPanel) {
+
+        }
     }
 
-    addUiPanel(panel: UiPanel, callback?: (err?: Error, doc?: UiPanel) => void) {
-        this.uiPanels.push(panel);
-        this.db.addUiPanel(panel, callback);
 
-        // return this.db.addUiPanel(panel, (err, doc) => {
-        //     if (!err)
-        //         this.uiPanels.push(panel);
-        //     callback(err, doc);
-        // });
-    };
+    getUiElementForNode(node: UiNode): UiElement {
+        var panel = this.getUiPanel(node.uiPanel);
+        if (panel)
+            return;
 
-    getUiPanels(callback?: (err?: Error, docs?: Array<UiPanel>) => void) {
-        return this.uiPanels;
-    };
-
-    getUiPanel(name: string, callback?: (err?: Error, doc?: UiPanel) => void) {
-        return this.uiPanels.some(p => p.name === name);
-    };
-
-    updateUiPanel(name: string, update: any, callback?: (err?: Error) => void) {
-        this.db.updateUiPanel(name, update, (err) => {
-            if (!err) {
-                callback(err);
-                return;
+        for (var s = 0; s < panel.subpanels.length; s++) {
+            var subpanel = panel.subpanels[s];
+            for (var e = 0; e < subpanel.uiElements.length; e++) {
+                var element = subpanel.uiElements[e];
+                if (element.containerId == node.container.id
+                    && element.nodeId == node.id)
+                    return element;
             }
-            this.getUiPanel(name, (err, doc) => {
-                if (err) {
-                    callback(err);
-                    return;
-                }
-                this.uiPanels = this.uiPanels.filter(p => p.name != name);
-                this.uiPanels.push(doc);
-            })
-        });
+        }
     };
 
-    removeUiPanel(name: string, callback?: (err?: Error) => void) {
-        this.uiPanels = this.uiPanels.filter(p => p.name != name);
-        this.db.removeUiPanel(name, callback);
+    getUiPanelForNode(node: UiNode): UiPanel {
+        return this.uiPanels.find(p => p.name === node.uiPanel);
     };
 
-    dropUiPanels(callback?: (err?: Error) => void) {
-        this.uiPanels = [];
-        this.db.dropUiPanels(callback);
+
+    getUiPanel(name: string): UiPanel {
+        return this.uiPanels.find(p => p.name === name);
     };
+
+    // getUiPanels(): Array<UiPanel> {
+    //     return this.uiPanels;
+    // };
+
+
+
+    // addUiPanel(panel: UiPanel, callback?: (err?: Error, doc?: UiPanel) => void) {
+    //     this.uiPanels.push(panel);
+    //     this.db.addUiPanel(panel, callback);
+
+    //     // return this.db.addUiPanel(panel, (err, doc) => {
+    //     //     if (!err)
+    //     //         this.uiPanels.push(panel);
+    //     //     callback(err, doc);
+    //     // });
+    // };
+
+
+    // updateUiPanel(name: string, update: any, callback?: (err?: Error) => void) {
+    //     this.db.updateUiPanel(name, update, (err) => {
+    //         if (!err) {
+    //             callback(err);
+    //             return;
+    //         }
+    //         this.db.getUiPanel(name, (err, doc) => {
+    //             if (err) {
+    //                 callback(err);
+    //                 return;
+    //             }
+    //             this.uiPanels = this.uiPanels.filter(p => p.name != name);
+    //             this.uiPanels.push(doc);
+    //         })
+    //     });
+    // };
+
+    // removeUiPanel(name: string, callback?: (err?: Error) => void) {
+    //     this.uiPanels = this.uiPanels.filter(p => p.name != name);
+    //     this.db.removeUiPanel(name, callback);
+    // };
+
+    // dropUiPanels(callback?: (err?: Error) => void) {
+    //     this.uiPanels = [];
+    //     this.db.dropUiPanels(callback);
+    // };
 
 
 }
