34bf06463edf396a60a14290a8b7490b6f9a7352
derwish-pro <derwish.pro@gmail.com>
2017-10-06 22:39:32 +0300

    dashboard toggle panels

--- src/client/dashboard/Dashboard.vue
+++ src/client/dashboard/Dashboard.vue
@@ -1,19 +1,30 @@
 <template lang='pug'>
 div
-  toolbar(:panels="panels" :selected="activePanel" @click="onClickToolbar")
+  toolbar(
+    :panels="panels" 
+    :selected="activePanel" 
+    :dashboardIsVisible="dashboardIsVisible",
+    :editorIsVisible="editorIsVisible",
+    @clickSidebar="onClickSidebar",
+    @clickToolbarDashboard="onClickToolbarDashboard",
+    @clickToolbarEditor="onClickToolbarEditor")
   main
     v-content.dash
       //- v-container(grid-list-xl)
-      div(v-if="activePanel")
+      div(v-if="dashboardIsVisible && editorIsVisible")
         v-layout(row wrap)
-          v-flex(v-if="activePanel" xs12 sm6 md4)
+          v-flex(xs12 sm6 md4)
             //- v-flex(v-if="activePanel" xs12 sm10 md6 offset-xs0 offset-sm1 offset-md3)
-            panel.panel(:name="activePanel")
+            panel.panel(v-if="activePanel" :name="activePanel")
+            div(v-else)
           v-flex(xs12 sm6 md8)
             editor.editor
-      div(v-else)
+      div(v-if="!dashboardIsVisible && editorIsVisible")
         editor.editor           
-
+      div(v-if="dashboardIsVisible && !editorIsVisible")
+        v-layout(row wrap)
+          v-flex(xs12 sm10 md6 offset-xs0 offset-sm1 offset-md3)
+            panel.panel(v-if="activePanel" :name="activePanel")
 
 </template>
 
@@ -29,12 +40,14 @@ export default {
       // { icon: "subscriptions", title: "Dashboard", name: "Dashboard" },
       // { icon: "trending_up", title: "Editor", name: "Editor" }
     ],
-    activePanel: "Test panel"
+    activePanel: "Test panel",
     // {
     //   icon: "subscriptions",
     //   title: "TestPanel",
     //   name: "TestPanel"
     // }
+    dashboardIsVisible: true,
+    editorIsVisible: true
   }),
   components: {
     panel: Panel,
@@ -62,12 +75,18 @@ export default {
     }
   },
   methods: {
-    onClickToolbar(panelName) {
+    onClickSidebar(panelName) {
       console.log(panelName);
 
       if (this.activePanel === panelName)
         this.$socket.emit("getUiPanel", panelName);
       else this.activePanel = panelName;
+    },
+    onClickToolbarDashboard() {
+      this.dashboardIsVisible = !this.dashboardIsVisible;
+    },
+    onClickToolbarEditor() {
+      this.editorIsVisible = !this.editorIsVisible;
     }
   }
 };
