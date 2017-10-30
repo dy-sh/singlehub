ea31d78d0bfd59333c3435e349620f533f229bb4
derwish-pro <derwish.pro@gmail.com>
2017-10-31 02:50:12 +0300

    ui slider

---
+++ src/client/dashboard/nodes/UiSliderNode.vue
@@ -0,0 +1,53 @@
+<template lang='pug'>
+  div
+    v-list(dense)
+      v-list-tile(v-if="stateReceived" height=300)
+        v-list-tile-content
+          v-list-tile-title {{uiElement.title}}
+          v-list-tile-sub-title {{uiElement.subtitle}}
+        v-list-tile-content.slider-parent
+          v-slider.slder(v-model="sliderValue")
+</template>
+
+
+<script>
+import getNodeStateMixin from "./mixins/getNodeState";
+import sendMessageToNodeeMixin from "./mixins/sendMessageToNode";
+
+export default {
+  mixins: [getNodeStateMixin, sendMessageToNodeeMixin],
+  props: ["uiElement"],
+  data() {
+    return {
+      sliderValue: 0,
+      state: null,
+      stateReceived: false
+    };
+  },
+  watch: {
+    state(val) {
+      console.log("state", val);
+      this.sliderValue = val;
+    },
+    sliderValue(val) {
+      console.log("sliderValue", val);
+
+      //rpevent loop sending
+      if (this.state != val) {
+        this.state = val;
+        this.sendMessageToNode({ state: this.state });
+      }
+    }
+  }
+};
+</script>
+
+<style scoped>
+.slider-parent {
+  height: 50px;
+}
+.slder {
+  /* top: -20px; */
+  padding-left: 12px !important;
+}
+</style>
--- src/nodes/nodes/ui/slider.ts
+++ src/nodes/nodes/ui/slider.ts
@@ -4,107 +4,51 @@
  */
 
 
-import { Node } from "../../node";
-import Utils from "../../utils";
-import { Side, Container } from "../../container";
 import { UiNode } from "./ui-node";
+import { Side, Container } from "../../container";
+import Utils from "../../utils";
 
-let template =
-    '<div class="ui attached clearing segment" id="node-{{id}}">\
-        <span id="nodeTitle-{{id}}"></span>\
-        <br />\
-        <div id="slider-{{id}}"></div>\
-    </div>';
-
-declare let noUiSlider;
 
 export class UiSliderNode extends UiNode {
-    UPDATE_INTERVAL = 100;
-
-    dataUpdated = false;
-
-    slider: HTMLElement;
 
     constructor() {
         super("Slider", "UiSliderNode");
 
         this.descriprion = "";
-        this.properties['state'] = 0;
-
-        this.settings["min"] = { description: "Min", value: 0, type: "number" };
-        this.settings["max"] = { description: "Max", value: 100, type: "number" };
-
+        this.addInput("input", "number");
         this.addOutput("output", "number");
+        this.UPDATE_INPUTS_INTERVAL = 100;
+        this.setState(0);
     }
 
     onAdded() {
         super.onAdded();
 
         if (this.side == Side.server)
-            this.setOutputData(0, this.properties['state']);
-
-        if (this.side == Side.dashboard) {
-
-            this.slider = $("#slider-" + this.id)[0];
-            noUiSlider.create(this.slider,
-                {
-                    start: 0,
-                    connect: 'lower',
-                    animate: false,
-                    range: {
-                        'min': this.settings["min"].value,
-                        'max': this.settings["max"].value
-                    }
-                });
-
-            let that = this;
-            (<any>this.slider).noUiSlider.on('slide', function () {
-                let val = (<any>that.slider).noUiSlider.get();
-                that.properties['state'] = val;
-                that.dataUpdated = true;
-            });
-
-            this.startSendingToServer();
-
-            this.onGetMessageToDashboardSide({ value: this.properties['state'] })
-        }
-    }
-
-
-    onAfterSettingsChange() {
-
-        if (this.side == Side.dashboard) {
-            (<any>$("#slider-" + this.id)[0]).noUiSlider.updateOptions({
-                range: {
-                    'min': this.settings["min"].value,
-                    'max': this.settings["max"].value
-                }
-            });
-        }
-    }
-
-
-    startSendingToServer() {
-        let that = this;
-        setInterval(function () {
-            if (that.dataUpdated) {
-                that.dataUpdated = false;
-                that.sendMessageToServerSide({ value: that.properties['state'] });
-            }
-        }, this.UPDATE_INTERVAL);
+            this.setOutputData(0, this.getState());
     }
 
     onGetMessageToServerSide(data) {
-        this.isRecentlyActive = true;
-        this.properties['state'] = data.value;
-        this.setOutputData(0, data.value);
-        this.sendIOValuesToEditor();
-        this.sendMessageToDashboardSide(data);
+        this.setValue(data.state);
     };
 
-    onGetMessageToDashboardSide(data) {
-        (<any>this.slider).noUiSlider.set(data.value);
+    onInputUpdated() {
+        let val = this.getInputData(0);
+        this.setValue(val);
     };
+
+    setValue(val) {
+        if (val > 100) val = 100;
+        if (val < 0) val = 0;
+
+        this.setOutputData(0, val);
+        this.sendIOValuesToEditor();
+        this.isRecentlyActive = true;
+
+
+        if (this.getState() != val)//prevent loop sending
+            this.setState(val);
+    }
 }
 
 Container.registerNodeType("ui/slider", UiSliderNode);
