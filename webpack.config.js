e8e2c4026f3a867cb5a2c079bc5ef2cd27b89014
derwish-pro <derwish.pro@gmail.com>
2017-10-31 02:14:44 +0300

    install webpack uglify fix

--- webpack.config.js
+++ webpack.config.js
@@ -91,12 +91,12 @@ if (process.env.NODE_ENV === 'production') {
         NODE_ENV: '"production"'
       }
     }),
-    new webpack.optimize.UglifyJsPlugin({
-      sourceMap: true,
-      compress: {
-        warnings: false
-      }
-    }),
+    // new webpack.optimize.UglifyJsPlugin({
+    //   sourceMap: true,
+    //   compress: {
+    //     warnings: false
+    //   }
+    // }),
     new webpack.LoaderOptionsPlugin({
       minimize: true
     })
