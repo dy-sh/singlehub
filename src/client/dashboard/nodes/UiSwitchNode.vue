724976fc3096d8e09d45263f9d6a31d2206bb61d
derwish-pro <derwish.pro@gmail.com>
2017-10-31 03:48:46 +0300

    ui switch

--- src/client/dashboard/nodes/UiSwitchNode.vue
+++ src/client/dashboard/nodes/UiSwitchNode.vue
@@ -1,11 +1,12 @@
 <template lang='pug'>
-  v-list-tile(v-if="stateReceived")
-    v-list-tile-content
-      v-list-tile-title {{uiElement.title}}
-      v-list-tile-sub-title {{uiElement.subtitle}}
-    v-spacer
-    v-list-tile-action
-      v-switch(v-model="state" color='grey lighten-3' @click='onClick')  
+  v-list(dense)
+    v-list-tile(v-if="stateReceived")
+      v-list-tile-content
+        v-list-tile-title {{uiElement.title}}
+        v-list-tile-sub-title {{uiElement.subtitle}}
+      v-spacer
+      v-list-tile-action
+        v-switch(v-model="state" color='grey lighten-3' @click='onClick')  
 </template>
 
 
