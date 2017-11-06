87690f2acddd2044942a2128388fe2d04fdce6ea
derwish-pro <derwish.pro@gmail.com>
2017-11-07 01:02:55 +0300

    root nodes component

---
+++ src/client/control-page/root-nodes/RootNodes.vue
@@ -0,0 +1,18 @@
+<template lang='pug'>
+    div
+      div(v-for="node in nodes")
+        component(:is="node")
+</template>
+
+
+<script>
+import RootNodes from "./list";
+
+export default {
+  data: () => ({
+    nodes: RootNodes
+  }),
+  components: RootNodes
+};
+</script>
+
---
+++ src/client/control-page/root-nodes/list.js
@@ -0,0 +1,10 @@
+import UiAudioNode from "./UiAudioNode.vue";
+import UiVoiceChromeNode from "./UiVoiceChromeNode.vue";
+import UiVoiceYandexNode from "./UiVoiceYandexNode.vue";
+
+
+export default {
+    UiAudioNode,
+    UiVoiceChromeNode,
+    UiVoiceYandexNode
+}
