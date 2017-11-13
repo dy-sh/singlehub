f76bda3fe535c26719e9000dff550c72bdf2279f
derwish-pro <derwish.pro@gmail.com>
2017-11-13 23:00:43 +0300

    textbox node

---
+++ src/client/dashboard/nodes/UiTextBoxNode.vue
@@ -0,0 +1,26 @@
+<template lang='pug'>
+  v-list(dense)
+    v-list-tile(v-if="stateReceived" height=400)
+      v-list-tile-content
+        v-list-tile-title {{uiElement.title}}
+        v-list-tile-sub-title {{uiElement.subtitle}}
+      v-list-tile-content  
+        v-text-field(v-model="state")
+</template>
+
+
+
+<script>
+import getNodeStateMixin from "./mixins/getNodeState";
+
+export default {
+  mixins: [getNodeStateMixin],
+  props: ["uiElement"],
+  data() {
+    return {
+      state: null,
+      stateReceived: false
+    };
+  }
+};
+</script>
