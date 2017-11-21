61a14ca9cd797b6c0e6b05f34d07377def167206
derwish-pro <derwish.pro@gmail.com>
2017-11-21 22:32:10 +0300

    textbox style

--- src/client/dashboard/nodes/UiTextBoxNode.vue
+++ src/client/dashboard/nodes/UiTextBoxNode.vue
@@ -1,26 +1,41 @@
 <template lang='pug'>
-  v-list(dense)
-    v-list-tile(v-if="stateReceived" height=400)
+  v-list.parrent(dense)
+    v-list-tile(v-if="stateReceived")
       v-list-tile-content
         v-list-tile-title {{uiElement.title}}
         v-list-tile-sub-title {{uiElement.subtitle}}
-      v-list-tile-content  
-        v-text-field(v-model="state")
+      v-list-tile-content.textfield
+        v-text-field(v-model="state" @input='onChange')
 </template>
 
 
 
 <script>
 import getNodeStateMixin from "./mixins/getNodeState";
+import sendMessageToNodeMixin from "./mixins/sendMessageToNode";
 
 export default {
-  mixins: [getNodeStateMixin],
+  mixins: [getNodeStateMixin, sendMessageToNodeMixin],
   props: ["uiElement"],
   data() {
     return {
       state: null,
       stateReceived: false
     };
+  },
+  methods: {
+    onChange() {
+      this.sendMessageToNode({ state: this.state });
+    }
   }
 };
 </script>
+
+<style scoped>
+.textfield {
+  height: 50px !important;
+}
+.parrent {
+  height: 60px !important;
+}
+</style>
