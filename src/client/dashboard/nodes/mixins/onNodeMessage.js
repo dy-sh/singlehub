e8f5ac4ea956425b94bc6c2301286dd68a31595f
derwish-pro <derwish.pro@gmail.com>
2017-11-27 21:09:32 +0300

    onNodeMessageMixin

--- src/client/dashboard/nodes/mixins/onNodeMessage.js
+++ src/client/dashboard/nodes/mixins/onNodeMessage.js
@@ -4,8 +4,10 @@ export default {
         this.$options.sockets.nodeMessageToDashboardSide = data => {
             // console.log(data);
             if (this.uiElement.cid === data.cid && this.uiElement.id === data.id) {
-                if (node['onNodeMessage']) {
-                    node['onNodeMessage'](data.message);
+                if (this['onNodeMessage']) {
+                    this.onNodeMessage(data.message);
+                } else {
+                    console.error("Cant receive message from server side. Please, add \"onNodeMessage\" method to dashboard vue-node, that use onNodeMessage mixin")
                 }
             };
         }
